116ffe664c58d03fd9d2295eba355564d15388d1
Spoorti R <spoorti452@gmail.com>
2026-04-29 01:01:07 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -47,8 +47,8 @@ allowed_origins = list({settings.FRONTEND_URL, "http://localhost:5173"})
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
